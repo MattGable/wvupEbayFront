57a95bcf87c6fafd4add5e5bcd3644f23ecac173
MattGable <matthewgable@gmail.com>
2016-10-17 22:47:45 -0400

    Added basic front end form. TODO: Fix propogation

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var app = express();
+
+//Listen on our specifed port or 3000
+var port = process.env.PORT || 3000;
+
+//TODO-public direcotry currently empty
+app.use(express.static(__dirname + './public'));
+
+//Template engine used and view directory
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+//TODO
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get('/', function(req, res) {
+    res.render('main');
+});
+
+
+//Listen on port number in var port
+app.listen(port, function() {
+    console.log('eBay Front test on port ' + port)
+});
 
 console.log('Hello');
